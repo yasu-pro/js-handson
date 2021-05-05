4e1c1762621fb0db9f8d9395e630bd29fc8359a4
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-05-05 18:51:34 +0900

    処理ごとにまとめる

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 "use strict";
 
 const ul = document.getElementById("js-list");
-
 const li = document.createElement("li");
-
 const a = document.createElement("a");
-
 const img = document.createElement("img");
 
 a.textContent = "これです。";
-
 a.setAttribute("href", "1.html");
-
 img.setAttribute("src", "bookmark.png");
-
 img.setAttribute("alt", "ブックマーク");
 
 ul.appendChild(li);
-
 li.appendChild(a);
-
 a.prepend(img);
