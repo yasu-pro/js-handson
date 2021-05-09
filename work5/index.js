77cf0360d4e030c97590ac4df730c05e2f8f9c4b
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-05-10 08:05:30 +0900

    [create] 課題5作成

--- work5/index.js
+++ work5/index.js
@@ -2,22 +2,67 @@
 
 const ul = document.getElementById("js-list");
 const fragment = document.createDocumentFragment();
-const items = [
-  { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
-  { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
-];
 
-items.forEach((item) => {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
-  const img = document.createElement("img");
+//ver1
+function getObj(items) {
+  return new Promise((resolve, reject) => {
+    resolve(
+      (items = [
+        {
+          to: "bookmark.html",
+          img: "1.png",
+          alt: "画像1",
+          text: "ブックマーク",
+        },
+        { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+      ])
+    );
+  });
+}
 
-  a.href = item.to;
-  a.textContent = item.text;
-  img.src = item.img;
-  img.alt = item.alt;
+getObj().then((items) => {
+  items.forEach((item) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
 
-  fragment.appendChild(li).appendChild(a).prepend(img);
+    a.href = item.to;
+    a.textContent = item.text;
+    img.src = item.img;
+    img.alt = item.alt;
+
+    fragment.appendChild(li).appendChild(a).prepend(img);
+  });
+
+  ul.appendChild(fragment);
 });
 
-ul.appendChild(fragment);
+//ver2
+const promise = new Promise((resolve, reject) => {
+  resolve([
+    {
+      to: "bookmark.html",
+      img: "1.png",
+      alt: "画像1",
+      text: "ブックマーク",
+    },
+    { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+  ]);
+});
+
+promise.then((items) => {
+  items.forEach((item) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    a.href = item.to;
+    a.textContent = item.text;
+    img.src = item.img;
+    img.alt = item.alt;
+
+    fragment.appendChild(li).appendChild(a).prepend(img);
+  });
+
+  ul.appendChild(fragment);
+});
