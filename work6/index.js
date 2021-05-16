434bebc3aedacd4fbcdb6eed8c83f740b8ba4fbf
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-05-16 22:52:26 +0900

    課題6 完了

---
+++ work6/index.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const ul = document.getElementById("js-list");
+const fragment = document.createDocumentFragment();
+
+function getObj() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([
+        {
+          to: "bookmark.html",
+          img: "1.png",
+          alt: "画像1",
+          text: "ブックマーク",
+        },
+        { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+      ]);
+    }, 3000);
+  });
+}
+
+getObj().then((items) => {
+  items.forEach((item) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    a.href = item.to;
+    a.textContent = item.text;
+    img.src = item.img;
+    img.alt = item.alt;
+
+    fragment.appendChild(li).appendChild(a).prepend(img);
+  });
+
+  ul.appendChild(fragment);
+});
