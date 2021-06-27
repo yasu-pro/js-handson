bfd46995338277f483e7444ef873a38b72d8c4d0
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-06-27 21:51:44 +0900

    fix: work8-2 Remove resolve in getObj function

--- work8-2/index.js
+++ work8-2/index.js
@@ -33,23 +33,8 @@ function getObj() {
 
     setTimeout(() => {
 
-      return reject(new Error("エラー"));
+      reject(new Error("エラー"));
 
-
-      resolve([
-        {
-          to: "bookmark.html",
-          img: "1.png",
-          alt: "画像1",
-          text: "ブックマーク",
-        },
-        {
-          to: "message.html",
-          img: "2.png",
-          alt: "画像2",
-          text: "メッセージ"
-        },
-      ]);
     }, 3000);
   });
 }
