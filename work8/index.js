42a25d2e08f548d20b298a303ad816f106e5c6af
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-06-27 21:49:24 +0900

    fix: remove resolve in getObj function

--- work8/index.js
+++ work8/index.js
@@ -1,56 +1,38 @@
 "use strict";
 
 function getObj() {
-  const ul = document.getElementById("js-list");
-  ul.style.backgroundImage = "url(./img/loading-circle.gif)";
-  ul.style.backgroundRepeat = "no-repeat";
-  ul.style.height = "100px";
+    const ul = document.getElementById("js-list");
+    ul.style.backgroundImage = "url(./img/loading-circle.gif)";
+    ul.style.backgroundRepeat = "no-repeat";
+    ul.style.height = "100px";
 
-  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
 
-    setTimeout(() => {
+        setTimeout(() => {
 
-      return reject(new Error("エラー"));
+            reject(new Error("エラー"));
 
-
-      resolve([
-        {
-          to: "bookmark.html",
-          img: "1.png",
-          alt: "画像1",
-          text: "ブックマーク",
-        },
-        {
-          to: "message.html",
-          img: "2.png",
-          alt: "画像2",
-          text: "メッセージ"
-        },
-      ],
-        ul.style.backgroundImage = "none",
-      );
-
-    }, 3000);
-  });
+        }, 3000);
+    });
 }
 
 getObj().then((items) => {
-  const ul = document.getElementById("js-list");
-  const fragment = document.createDocumentFragment();
-  items.forEach((item) => {
-    const li = document.createElement("li");
-    const a = document.createElement("a");
-    const img = document.createElement("img");
+    const ul = document.getElementById("js-list");
+    const fragment = document.createDocumentFragment();
+    items.forEach((item) => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const img = document.createElement("img");
 
-    a.href = item.to;
-    a.textContent = item.text;
-    img.src = item.img;
-    img.alt = item.alt;
+        a.href = item.to;
+        a.textContent = item.text;
+        img.src = item.img;
+        img.alt = item.alt;
 
-    fragment.appendChild(li).appendChild(a).prepend(img);
-  });
-  ul.appendChild(fragment);
+        fragment.appendChild(li).appendChild(a).prepend(img);
+    });
+    ul.appendChild(fragment);
 })
-  .catch((error) => {
-    console.error(error);
-  })
+    .catch((error) => {
+        console.error(error);
+    })
