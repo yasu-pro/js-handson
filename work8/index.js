25b197c33b20199bf7ddfcda7115852a318fd085
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-06-27 14:16:12 +0900

    style: resolveをコメントアウト

--- work8/index.js
+++ work8/index.js
@@ -1,96 +1,16 @@
-// "use strict";
+"use strict";
 
-// function getObj() {
-//   const ul = document.getElementById("js-list");
-//   ul.style.backgroundImage = "url(./img/loading-circle.gif)";
-//   ul.style.backgroundRepeat = "no-repeat";
-//   ul.style.height = "100px";
-
-//   return new Promise((resolve, reject) => {
-
-//     setTimeout(() => {
-
-//       return reject(new Error("エラー"));
-
-
-//       resolve([
-//         {
-//           to: "bookmark.html",
-//           img: "1.png",
-//           alt: "画像1",
-//           text: "ブックマーク",
-//         },
-//         {
-//           to: "message.html",
-//           img: "2.png",
-//           alt: "画像2",
-//           text: "メッセージ"
-//         },
-//       ],
-//         ul.style.backgroundImage = "none",
-//       );
-
-//     }, 3000);
-//   });
-// }
-
-// getObj().then((items) => {
-//   const ul = document.getElementById("js-list");
-//   const fragment = document.createDocumentFragment();
-//   items.forEach((item) => {
-//     const li = document.createElement("li");
-//     const a = document.createElement("a");
-//     const img = document.createElement("img");
-
-//     a.href = item.to;
-//     a.textContent = item.text;
-//     img.src = item.img;
-//     img.alt = item.alt;
-
-//     fragment.appendChild(li).appendChild(a).prepend(img);
-//   });
-//   ul.appendChild(fragment);
-// })
-//   .catch((error) => {
-//     console.error(error);
-//   })
-
-loading();
-
-function loading() {
+function getObj() {
   const ul = document.getElementById("js-list");
   ul.style.backgroundImage = "url(./img/loading-circle.gif)";
   ul.style.backgroundRepeat = "no-repeat";
   ul.style.height = "100px";
-}
 
-function createElement(items) {
-  const ul = document.getElementById("js-list");
-  const fragment = document.createDocumentFragment();
-  ul.style.backgroundImage = "none",
-
-    items.forEach((item) => {
-      const li = document.createElement("li");
-      const a = document.createElement("a");
-      const img = document.createElement("img");
-
-      a.href = item.to;
-      a.textContent = item.text;
-      img.src = item.img;
-      img.alt = item.alt;
-
-      fragment.appendChild(li).appendChild(a).prepend(img);
-    });
-  ul.appendChild(fragment);
-}
-
-
-function getObj() {
   return new Promise((resolve, reject) => {
 
     setTimeout(() => {
 
-      // return reject(new Error("エラー"));
+      return reject(new Error("エラー"));
 
 
       resolve([
@@ -106,13 +26,30 @@ function getObj() {
           alt: "画像2",
           text: "メッセージ"
         },
-      ]);
+      ],
+        ul.style.backgroundImage = "none",
+      );
+
     }, 3000);
   });
 }
 
 getObj().then((items) => {
-  createElement(items);
+  const ul = document.getElementById("js-list");
+  const fragment = document.createDocumentFragment();
+  items.forEach((item) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+
+    a.href = item.to;
+    a.textContent = item.text;
+    img.src = item.img;
+    img.alt = item.alt;
+
+    fragment.appendChild(li).appendChild(a).prepend(img);
+  });
+  ul.appendChild(fragment);
 })
   .catch((error) => {
     console.error(error);
