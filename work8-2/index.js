3e54934d57732e9361cac824a6c92c9b55783031
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-06-27 14:20:32 +0900

    refactor: thenの中でDOMを生成しないパターン work8-2

---
+++ work8-2/index.js
@@ -0,0 +1,62 @@
+loading();
+
+function loading() {
+  const ul = document.getElementById("js-list");
+  ul.style.backgroundImage = "url(./img/loading-circle.gif)";
+  ul.style.backgroundRepeat = "no-repeat";
+  ul.style.height = "100px";
+}
+
+function createElement(items) {
+  const ul = document.getElementById("js-list");
+  const fragment = document.createDocumentFragment();
+  ul.style.backgroundImage = "none",
+
+    items.forEach((item) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const img = document.createElement("img");
+
+      a.href = item.to;
+      a.textContent = item.text;
+      img.src = item.img;
+      img.alt = item.alt;
+
+      fragment.appendChild(li).appendChild(a).prepend(img);
+    });
+  ul.appendChild(fragment);
+}
+
+
+function getObj() {
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+
+      return reject(new Error("エラー"));
+
+
+      resolve([
+        {
+          to: "bookmark.html",
+          img: "1.png",
+          alt: "画像1",
+          text: "ブックマーク",
+        },
+        {
+          to: "message.html",
+          img: "2.png",
+          alt: "画像2",
+          text: "メッセージ"
+        },
+      ]);
+    }, 3000);
+  });
+}
+
+getObj().then((items) => {
+  createElement(items);
+})
+  .catch((error) => {
+    console.error(error);
+  })
