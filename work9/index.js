49993a52836819fb04dfcd9c23f299d645be5cda
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-07-04 21:49:15 +0900

    update Assign an object to a variable

--- work9/index.js
+++ work9/index.js
@@ -1,43 +1,61 @@
 "use strict";
 
 const ul = document.getElementById("js-list");
+const obj = [{
+  to: "bookmark.html",
+  img: "1.png",
+  alt: "画像1",
+  text: "ブックマーク",
+},
+{
+  to: "message.html",
+  img: "2.png",
+  alt: "画像2",
+  text: "メッセージ"
+},
+]
 
 function createElement(items) {
-    const fragment = document.createDocumentFragment();
-    items.forEach((item) => {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        const img = document.createElement("img");
+  const fragment = document.createDocumentFragment();
+  items.forEach((item) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
 
-        a.href = item.to;
-        a.textContent = item.text;
-        img.src = item.img;
-        img.alt = item.alt;
+    a.href = item.to;
+    a.textContent = item.text;
+    img.src = item.img;
+    img.alt = item.alt;
 
-        fragment.appendChild(li).appendChild(a).prepend(img);
-    });
-    ul.appendChild(fragment);
+    fragment.appendChild(li).appendChild(a).prepend(img);
+  });
+  ul.appendChild(fragment);
 }
 
 function getObj() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject(new Error("エラー"));
-        }, 3000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(obj);
+
+
+      reject(new Error("エラー"));
+
+    }, 3000);
+  })
 }
 
 getObj().then((items) => {
-    createElement(items);
+  createElement(items);
+  ul.style.backgroundImage = "none";
 })
-    .catch((error) => {
-        console.error(error);
-    })
+  .catch((error) => {
+    console.error(error);
+  });
 
 function loading() {
-    ul.style.backgroundImage = "url(./img/loading-circle.gif)";
-    ul.style.backgroundRepeat = "no-repeat";
-    ul.style.height = "100px";
+  ul.style.backgroundImage = "url(./img/loading-circle.gif)";
+  ul.style.backgroundRepeat = "no-repeat";
+  ul.style.height = "100px";
 }
 
 loading();
