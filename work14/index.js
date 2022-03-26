b9c90f36eee2d9eac6cbd45d3ee3682852e8ea68
yasu-pro <cb1-y.s@docomo.ne.jp>
2022-03-26 16:43:52 +0900

    fix: Change response error to console output

--- work14/index.js
+++ work14/index.js
@@ -2,17 +2,14 @@
 const modalBtn = document.getElementById("js_modalButton");
 const modal = document.getElementById("js_modal");
 const requestBtn = document.getElementById("js_requestButton");
-const wrap = document.getElementById("js_wrap");
 
-// const url = "https://mocki.io/v1/d6da0b8a-3546-419e-aaec-05f3a247c6d0";
+const url = "https://mocki.io/v1/d6da0b8a-3546-419e-aaec-05f3a247c6d0";
 // 間違っているURLの場合↓
 // const url = "https://myjson.dit.upm.es/api/bins/ほげほげajy3";
 // 空の配列の場合↓
 // const url = "https://mocki.io/v1/242b685f-a3d7-45a8-aeca-0376bd495b89";
 // 404エラーの場合↓
 // const url = "https://httpstat.us/404";
-// 503エラーの場合↓
-const url = "https://httpstat.us/503";
 
 // 下記は、myjson繋がらない時の固定値
 // const url = {
@@ -38,15 +35,13 @@ async function getData() {
     if (response.ok) {
       const json = await response.json();
       return json;
-    } else if (response.status >= 400) {
-      console.error(response.statusText);
-      wrap.textContent = `${"ページは存在しないようです：" + response.status}`;
+    } else {
+      console.error(`${response.status}:${response.statusText}`);
     }
     // 下記は、固定値をそのままpromiseの返り値とする
     // const json = url
     // return json
   } catch (e) {
-    console.error(e);
     throw new Error(e);
   }
 }
@@ -54,6 +49,7 @@ async function getData() {
 async function init() {
   renderLoading();
   let listData;
+  const wrap = document.getElementById("js_wrap");
 
   try {
     listData = await getData();
