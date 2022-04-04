e46c976c0f6d0f11e7afb62c1db8cce0e9c3b6ce
yasu-pro <cb1-y.s@docomo.ne.jp>
2022-04-04 14:17:10 +0900

    feat: Create functions to show and hide loading

--- work16/index.js
+++ work16/index.js
@@ -1,11 +1,17 @@
-// const requestURL = "https://mocki.io/v1/764bd14c-1995-4f85-88f1-eb30bbfcefd8"
+// loadingとhideloading関数を作成
+// 取得したデータからニュースのカテゴリーを作成
+
+// ニュースの記事を表示
+// コメントがあれば表示
+
+const requestURL = "https://mocki.io/v1/764bd14c-1995-4f85-88f1-eb30bbfcefd8"
 
 // 間違っているURLの場合↓
 // const requestURL = "https://myjson.dit.upm.es/api/bins/ほげほげajy3";
 // 空の配列の場合↓
 // const requestURL = "https://mocki.io/v1/242b685f-a3d7-45a8-aeca-0376bd495b89";
 // 503エラーの場合↓
-const requestURL = "https://httpstat.us/503";
+// const requestURL = "https://httpstat.us/503";
 
 const request = async () => {
     try {
@@ -22,17 +28,23 @@ const request = async () => {
 }
 
 const init = async () => {
+    renderLoadingImg();
+
     let responseJsonData;
     const p = document.createElement("p");
 
     try {
         responseJsonData = await request();
+        console.log(responseJsonData);
         if (!responseJsonData) {
             return;
         }
     } catch (e) {
         p.textContent = e.message;
         document.body.prepend(p);
+    }
+    finally {
+        hideLoadingImg();
     }
 
     if (!responseJsonData.data.length) {
@@ -43,5 +55,21 @@ const init = async () => {
     // return responseJsonData.data;
 }
 
+const renderLoadingImg = () => {
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+
+    div.classList = "loading_wrap";
+    div.id = "js_loading_wrap"
+    img.src = "./img/loading-circle.gif";
+    img.alt = "ローディング画像"
+
+    div.appendChild(img);
+    document.body.prepend(div);
+}
+
+const hideLoadingImg = () => {
+    document.getElementById("js_loading_wrap").remove();
+}
 
 console.log(init());
